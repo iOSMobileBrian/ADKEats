85b5b6411808d56da243de62429310f75b7286a5
Brian Surface <iosmobilebrian@gmail.com>
2020-04-19 10:21:27 -0400

    Initial Commit

---
+++ ADKEats/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  ADKEats
+//
+//  Created by Brian Surface on 4/19/20.
+//  Copyright © 2020 Adamantium Mettle. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ADKEats/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  ADKEats
+//
+//  Created by Brian Surface on 4/19/20.
+//  Copyright © 2020 Adamantium Mettle. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
